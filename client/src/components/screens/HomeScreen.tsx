184f61841d2250b25cd1ba0e52b498b20fcb5e3e
Stayingfalse <uns@cezz.co.uk>
2026-05-04 04:20:04 +0100

    Fix closing div tag in HomeScreen component

--- client/src/components/screens/HomeScreen.tsx
+++ client/src/components/screens/HomeScreen.tsx
@@ -306,7 +306,7 @@ export default function HomeScreen({
       <p className="text-gray-600 text-xs text-center max-w-sm px-2">
         Ingenious is a board game originally designed by Reiner Knizia and published by Sophisticated Games Ltd. / Rio Grande Games. This fan-made web implementation is not affiliated with or endorsed by the original creators or rights holders.
       </p>
-    </div>
     <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="cezzt" data-description="Support me on Buy me a coffee!" data-message="Help keep this apps servers running and buy me a coffee. " data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
-  )
+   </div>
+      )
 }
